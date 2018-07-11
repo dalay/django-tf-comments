1f92bcbcab92df1bb869a0e816d40c7b4277a73e
dalay <dalay@drupalka.ru>
2018-07-11 16:33:16 +0300

    url to path(django 2)

--- urls.py
+++ urls.py
@@ -1,26 +1,26 @@
-from django.conf.urls import url
+from django.urls import path
 from comments import views
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 
 
 urlpatterns = [
-    url(r'^new/(?P<content_type_id>\d+)/(?P<object_id>\d+)/$',
-        views.CommentCreate.as_view(), name='comment-new'),
-    url(r'^edit/(?P<pk>\d+)/$',
-        login_required(views.CommentUpdate.as_view()),
-        name='comment-edit'),
-    url(r'^delete/(?P<pk>\d+)/$',
-        login_required(views.CommentDelete.as_view()), name='comment-delete'),
-    url(r'^reply/(?P<pk>[\d-]+)/$', views.CommentReply.as_view(),
-        name='comment-reply'),
-    url(r'^unpublished/$', views.UnpublishedCommentsList.as_view(),
-        name='comments-unpubleshed-list'),
-    url(r'^toggle-status/(?P<pk>\d+)/$',
-        staff_member_required(views.comment_status_toggle),
-        name='comment-status-toggle'),
-
-    # url(r'^(?P<pk>[\d-]+)/$', login_required(views.AdvertEdit.as_view()), name='comment-detail',
-    # url(r'^(?P<pk>[\d-]+)/edit', login_required(views.AdvertEdit.as_view()), name='comment-edit'),
-    # url(r'^(?P<pk>[\d-]+)/delete', login_required(views.AdvertEdit.as_view()), name='comment-delete'),
+    # Adding a new comment.
+    path('new/<int:content_type_id>/<int:object_id>/',
+         views.CommentCreate.as_view(), name='comment-new'),
+    # Editing a comment (if there are rights for this).
+    path('edit/<int:pk>/',
+         login_required(views.CommentUpdate.as_view()), name='comment-edit'),
+    # Delete a comment.
+    path('delete/<int:pk>/',
+         login_required(views.CommentDelete.as_view()), name='comment-delete'),
+    # Reply to comment.
+    path('reply/<int:pk>/', views.CommentReply.as_view(), name='comment-reply'),
+    # A page with a list of unpublished comments.
+    path('unpublished/', views.UnpublishedCommentsList.as_view(),
+         name='comments-unpubleshed-list'),
+    # Switching comment status (published/unpublished).
+    path('toggle-status/<int:pk>/',
+         staff_member_required(views.comment_status_toggle),
+         name='comment-status-toggle'),
 ]
