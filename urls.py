81946eb0e9f101fc5069198d2acdf5c7158bc00f
dalay <dalay@drupalka.ru>
2018-07-11 17:14:28 +0300

    urls fix

--- urls.py
+++ urls.py
@@ -4,23 +4,24 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 
 
+app_name = 'comments'
 urlpatterns = [
     # Adding a new comment.
     path('new/<int:content_type_id>/<int:object_id>/',
-         views.CommentCreate.as_view(), name='comment-new'),
+         views.CommentCreate.as_view(), name='new'),
     # Editing a comment (if there are rights for this).
     path('edit/<int:pk>/',
-         login_required(views.CommentUpdate.as_view()), name='comment-edit'),
+         login_required(views.CommentUpdate.as_view()), name='edit'),
     # Delete a comment.
     path('delete/<int:pk>/',
-         login_required(views.CommentDelete.as_view()), name='comment-delete'),
+         login_required(views.CommentDelete.as_view()), name='delete'),
     # Reply to comment.
-    path('reply/<int:pk>/', views.CommentReply.as_view(), name='comment-reply'),
+    path('reply/<int:pk>/', views.CommentReply.as_view(), name='reply'),
     # A page with a list of unpublished comments.
     path('unpublished/', views.UnpublishedCommentsList.as_view(),
-         name='comments-unpubleshed-list'),
+         name='unpubleshed-list'),
     # Switching comment status (published/unpublished).
     path('toggle-status/<int:pk>/',
          staff_member_required(views.comment_status_toggle),
-         name='comment-status-toggle'),
+         name='status-toggle'),
 ]
